11430e05bd8ad8c4c84ceb69a402f67cfa497f13
TechTastic <74630543+TechTastic@users.noreply.github.com>
2022-09-27 22:20:05 -0500

    Initial commit

---
+++ src/main/java/net/techtastic/divineright/DivineRight.java
@@ -0,0 +1,14 @@
+package net.techtastic.divineright;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DivineRight implements ModInitializer {
+	public static final String MOD_ID = "divineright";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/techtastic/divineright/DivineRightClient.java
@@ -0,0 +1,10 @@
+package net.techtastic.divineright;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DivineRightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
